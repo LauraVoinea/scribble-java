f31396155a395bdd4bc9c2c7c4ef46a75ca17282
rhu1 <r.z.h.hu@herts.ac.uk>
2020-08-04 21:46:11 +0100

    [assrt] Minor.

--- scribble-assertions/src/main/java/org/scribble/ext/assrt/core/visit/global/AssrtCoreGTypeInliner.java
+++ scribble-assertions/src/main/java/org/scribble/ext/assrt/core/visit/global/AssrtCoreGTypeInliner.java
@@ -17,9 +17,6 @@ import org.scribble.core.job.Core;
 import org.scribble.core.lang.SubprotoSig;
 import org.scribble.core.type.kind.Global;
 import org.scribble.core.type.name.RecVar;
-import org.scribble.core.type.session.Do;
-import org.scribble.ext.assrt.core.type.session.AssrtCoreSType;
-import org.scribble.ext.assrt.core.type.session.NoSeq;
 import org.scribble.ext.assrt.core.type.session.global.AssrtCoreGType;
 import org.scribble.ext.assrt.core.visit.AssrtCoreSTypeInliner;
 
@@ -29,33 +26,6 @@ public class AssrtCoreGTypeInliner extends AssrtCoreSTypeInliner<Global,
 	public AssrtCoreGTypeInliner(Core core)
 	{
 		super(core);
-	}
-
-	@Override
-	public AssrtCoreSType<Global, AssrtCoreGType> visitDo(Do<Global, 
-			NoSeq<Global>> n)
-	{
-		throw new RuntimeException("Deprecated for " + getClass() + ":\n" + n);
-	/*
-		AssrtCoreSTypeFactory tf = (AssrtCoreSTypeFactory) this.core.config.tf;
-		AssrtCoreGDo n1 = (AssrtCoreGDo) n;
-		ProtoName<Global> fullname = n.proto;
-		SubprotoSig sig = new SubprotoSig(fullname, n.roles, n.args);  // FIXME: assrt subproto sig with state vars
-		RecVar rv = getInlinedRecVar(sig);
-		if (hasSig(sig))
-		{
-			return tf.global.AssrtCoreGRecVar(n.getSource(), rv, n);
-		}
-		pushSig(sig);
-		GProtocol g = this.core.getContext().getIntermediate(fullname);
-		Substitutor<Global, GSeq> subs = this.core.config.vf.Substitutor(g.roles,
-				n.roles, g.params, n.args);
-		//GSeq inlined = (GSeq) g.def.visitWithNoEx(subs).visitWithNoEx(this);
-		GSeq inlined = visitSeq(subs.visitSeq(g.def));
-				// i.e. returning a GSeq -- rely on parent GSeq to inline
-		popSig();
-		return this.core.config.tf.global.GRecursion(null, rv, inlined);
-	*/
 	}
 
 	// For public
