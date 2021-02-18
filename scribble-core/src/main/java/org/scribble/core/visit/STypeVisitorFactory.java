63fa8768a00a9fac7ae623bce20007ccf2778d84
rhu1 <r.z.h.hu@herts.ac.uk>
2021-02-18 01:01:05 +0000

    Minor

--- scribble-core/src/main/java/org/scribble/core/visit/STypeVisitorFactory.java
+++ scribble-core/src/main/java/org/scribble/core/visit/STypeVisitorFactory.java
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.scribble.core.visit;
 
-import java.util.List;
+package org.scribble.core.visit;
 
 import org.scribble.core.type.kind.NonRoleParamKind;
 import org.scribble.core.type.kind.ProtoKind;
@@ -26,33 +25,33 @@ import org.scribble.core.type.session.Seq;
 import org.scribble.core.visit.global.GTypeVisitorFactory;
 import org.scribble.core.visit.local.LTypeVisitorFactory;
 
+import java.util.List;
+
 // Not an interface due to fields
-public abstract class STypeVisitorFactory
-{
-	public final GTypeVisitorFactory global;
-	public final LTypeVisitorFactory local;
-	
-	public STypeVisitorFactory(GTypeVisitorFactory global,
-			LTypeVisitorFactory local)
-	{
-		this.global = global;
-		this.local = local;
-	}
+public abstract class STypeVisitorFactory {
 
-	public <K extends ProtoKind, B extends Seq<K, B>> Substitutor<K, B> 
-			Substitutor(List<Role> rold, List<Role> rnew,
-					List<MemberName<? extends NonRoleParamKind>> aold,
-					List<Arg<? extends NonRoleParamKind>> anew)
-	{
-		return Substitutor(rold, rnew, aold, anew, false);
-	}
+    public final GTypeVisitorFactory global;
+    public final LTypeVisitorFactory local;
 
-	public abstract <K extends ProtoKind, B extends Seq<K, B>> Substitutor<K, B> 
-			Substitutor(List<Role> rold, List<Role> rnew,
-					List<MemberName<? extends NonRoleParamKind>> aold,
-					List<Arg<? extends NonRoleParamKind>> anew, boolean passive);
+    public STypeVisitorFactory(GTypeVisitorFactory global,
+                               LTypeVisitorFactory local) {
+        this.global = global;
+        this.local = local;
+    }
 
-	public abstract <K extends ProtoKind, B extends Seq<K, B>> RecPruner<K, B> 
-			RecPruner();
+    // Default passive to false
+    public <K extends ProtoKind, B extends Seq<K, B>> Substitutor<K, B>
+    Substitutor(List<Role> rold, List<Role> rnew,
+                List<MemberName<? extends NonRoleParamKind>> aold,
+                List<Arg<? extends NonRoleParamKind>> anew) {
+        return Substitutor(rold, rnew, aold, anew, false);
+    }
 
+    public abstract <K extends ProtoKind, B extends Seq<K, B>> Substitutor<K, B>
+    Substitutor(List<Role> rold, List<Role> rnew,
+                List<MemberName<? extends NonRoleParamKind>> aold,
+                List<Arg<? extends NonRoleParamKind>> anew, boolean passive);
+
+    public abstract <K extends ProtoKind, B extends Seq<K, B>>
+    RecPruner<K, B> RecPruner();
 }
