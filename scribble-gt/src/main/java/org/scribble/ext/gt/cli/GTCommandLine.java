03574e564a8148ef1ac2fe71172dd6e5f0f405b5
rhu1 <r.hu@qmul.ac.uk>
2023-01-30 18:52:00 +0000

    Minor

--- scribble-gt/src/main/java/org/scribble/ext/gt/cli/GTCommandLine.java
+++ scribble-gt/src/main/java/org/scribble/ext/gt/cli/GTCommandLine.java
@@ -32,22 +32,22 @@ import java.util.stream.Collectors;
 
 public class GTCommandLine extends CommandLine {
 
-	private Job job;
+    private Job job;
 
-	public GTCommandLine(String... args) {
-		super(args);
-	}
+    public GTCommandLine(String... args) {
+        super(args);
+    }
 
-	public static void main(String[] args)
-			throws CommandLineException, AntlrSourceException {
-		//Map<String, String> y = Stream.of("abc").collect(Collectors.toUnmodifiableMap(x -> x, x -> x));
-		//y.put("def", "abc");
-		//Stream.of("abc").collect(Collectors.toUnmodifiableList()).remove(0);
+    public static void main(String[] args)
+            throws CommandLineException, AntlrSourceException {
+        //Map<String, String> y = Stream.of("abc").collect(Collectors.toUnmodifiableMap(x -> x, x -> x));
+        //y.put("def", "abc");
+        //Stream.of("abc").collect(Collectors.toUnmodifiableList()).remove(0);
 
-		GTCommandLine cl = new GTCommandLine(args);
-		cl.run();
-		cl.gtRun();
-	}
+        GTCommandLine cl = new GTCommandLine(args);
+        cl.run();
+        cl.gtRun();
+    }
 
 	/*// A Scribble extension should override as appropriate
 	protected Main newMain() throws ScribParserException, ScribException
@@ -71,58 +71,58 @@ public class GTCommandLine extends CommandLine {
 		}
 	}*/
 
-	protected GTMain main;
+    protected GTMain main;
 
-	// Duplicated from AssrtCommandLine
-	// Based on CommandLine.newMainContext
-	@Override
-	protected Main newMain() throws ScribParserException, ScribException
-	{
+    // Duplicated from AssrtCommandLine
+    // Based on CommandLine.newMainContext
+    @Override
+    protected Main newMain() throws ScribParserException, ScribException {
 		/*if (!hasFlag(AssrtCLFlags.ASSRT_CORE_FLAG))
 		{
 			return super.newMain();
 		}*/
-		//AssrtCoreArgs args = newCoreArgs();
-		CoreArgs args = newCoreArgs();
-		List<Path> impaths = parseImportPaths();
-		ResourceLocator locator = new DirectoryResourceLocator(impaths);
-		Path mainpath = parseMainPath();
-		this.main = new GTMain(locator, mainpath, args);
-		return main;
-	}
+        //AssrtCoreArgs args = newCoreArgs();
+        CoreArgs args = newCoreArgs();
+        List<Path> impaths = parseImportPaths();
+        ResourceLocator locator = new DirectoryResourceLocator(impaths);
+        Path mainpath = parseMainPath();
+        this.main = new GTMain(locator, mainpath, args);
+        return main;
+    }
 
-	protected void gtRun() {
-		Core core = this.job.getCore();
-		CoreContext c = core.getContext();
+    protected void gtRun() {
+        Core core = this.job.getCore();
+        CoreContext c = core.getContext();
 
-		Map<ModuleName, Module> parsed = this.main.getParsedModules();
-		System.out.println("\n-----\n");
-		System.out.println(parsed.keySet());
+        Map<ModuleName, Module> parsed = this.main.getParsedModules();
+        System.out.println("\n-----\n");
+        System.out.println(parsed.keySet());
 
-		for (ModuleName n : parsed.keySet()) {
-			Module m = parsed.get(n);
-			for (GProtoDecl g : m.getGProtoDeclChildren()) {
-				System.out.println(g);
+        for (ModuleName n : parsed.keySet()) {
+            Module m = parsed.get(n);
+            for (GProtoDecl g : m.getGProtoDeclChildren()) {
+                System.out.println(g);
 
-				GProtoDef defChild = g.getDefChild();
-				GInteractionSeq interactSeqChild = defChild.getBlockChild().getInteractSeqChild();
-				GTGType translate = new GTGTypeTranslator2().translate(interactSeqChild);
+                GProtoDef defChild = g.getDefChild();
+                GInteractionSeq interactSeqChild = defChild.getBlockChild().getInteractSeqChild();
+                GTGType translate = new GTGTypeTranslator2().translate(interactSeqChild);
 
-				System.out.println("---");
+                System.out.println("---");
 
-				for (Role r : g.getRoles()) {
-					System.out.println("project onto " + r + ": " + translate.project(r));
-				}
+                for (Role r : g.getRoles()) {
+                    System.out.println("project onto " + r + ": " + translate.project(r));
+                }
 
-				System.out.println("---");
+                System.out.println("---");
 
-				if (!translate.isSinglePointed()) {
-					System.err.println("Not single pointed: " + translate);
-				} else {
-					foo(core, "", translate, 0);
-				}
-			}
-		}
+                if (!translate.isSinglePointed()) {
+                    System.err.println("Not single pointed: " + translate);
+                } else {
+                    System.out.println("Initial g = " + translate);
+                    foo(core, "", translate, 0);
+                }
+            }
+        }
 
 		/*System.out.println("\n-----\n");
 		for (ProtoName<Global> p : c.getParsedFullnames()) {
@@ -132,26 +132,26 @@ public class GTCommandLine extends CommandLine {
 			System.out.println();
 			foo(core, "", g);
 		}*/
-	}
+    }
 
-	private void foo(Core core, String indent, GTGType g, int count) {
-		if (!g.isGood()) {
-			System.err.println("Not good: " + g);
-			System.exit(0);
-		}
-		if (!g.isCoherent()) {
-			System.err.println("Not coherent: " + g);
-			System.exit(0);
-		}
-		if (count > 20) {
-			System.err.println("Pruned.");
-			return;
-		}
-		GTSModelFactory mf = (GTSModelFactory) core.config.mf.global;
+    private void foo(Core core, String indent, GTGType g, int count) {
+        if (!g.isGood()) {
+            System.err.println("Not good: " + g);
+            System.exit(0);
+        }
+        if (!g.isCoherent()) {
+            System.err.println("Not coherent: " + g);
+            System.exit(0);
+        }
+        if (count > 20) {
+            System.err.println("Pruned.");
+            return;
+        }
+        GTSModelFactory mf = (GTSModelFactory) core.config.mf.global;
 
-		Theta theta = new Theta(g.getTimeoutIds());
+        Theta theta = new Theta(g.getTimeoutIds());
 
-		Set<SAction> as = g.getActs(mf, theta);
+        Set<SAction> as = g.getActs(mf, theta);
 		/*System.out.println(as);
 		SAction a = as.iterator().next();
 		GTGType g1 = g.step(a).get();  // a in as so step is non-empty
@@ -169,29 +169,29 @@ public class GTCommandLine extends CommandLine {
 		System.out.println(indent + "a = " + a);
 		System.out.println(indent + "g = " + g1);*/
 
-		for (SAction a : as) {
-			//System.out.println("bbb: " + g + " ,, " + a);
+        for (SAction a : as) {
+            //System.out.println("bbb: " + g.getClass() + " ,, " + g + " ,, " + a);
 
-			GTGType g1 = g.step(theta, a).get().right;  // a in as so step is non-empty
-			System.out.println(indent + "a = " + a);
-			System.out.println(indent + "g = " + g1);
-			if (!g1.equals(GTGEnd.END)) {
-				foo(core, indent + "    ", g1, count++);
-			}
-		}
-	}
+            GTGType g1 = g.step(theta, a).get().right;  // a in as so step is non-empty
+            System.out.println(indent + "a = " + a);
+            System.out.println(indent + "g = " + g1);
+            if (!g1.equals(GTGEnd.END)) {
+                foo(core, indent + "    ", g1, count++);
+            }
+        }
+    }
 
-	// FIXME refactor base scribble CLI methods (e.g., access Job)
-	protected void runTasks()
-			throws AntlrSourceException, ScribParserException, CommandLineException {
-		Main mc = newMain();  // Represents current instance of tooling for given CL args
-		Job job = mc.newJob();  // A Job is some series of passes performed on each Module in the MainContext (e.g., cf. Job::runVisitorPass)
-		ScribException err = null;
-		try {
-			doValidationTasks(job);
-		} catch (ScribException x) {
-			err = x;
-		}
+    // FIXME refactor base scribble CLI methods (e.g., access Job)
+    protected void runTasks()
+            throws AntlrSourceException, ScribParserException, CommandLineException {
+        Main mc = newMain();  // Represents current instance of tooling for given CL args
+        Job job = mc.newJob();  // A Job is some series of passes performed on each Module in the MainContext (e.g., cf. Job::runVisitorPass)
+        ScribException err = null;
+        try {
+            doValidationTasks(job);
+        } catch (ScribException x) {
+            err = x;
+        }
 		/*for (Pair<String, String[]> a : this.args) {
 			CLFlag flag = this.flags.explicit.get(a.left);  // null for CLFlags.MAIN_MOD_FLAG
 			if (a.left.equals(CLFlags.MAIN_MOD_FLAG) || !flag.enact) {
@@ -219,6 +219,6 @@ public class GTCommandLine extends CommandLine {
 			throw err;
 		}*/
 
-		this.job = job;
-	}
+        this.job = job;
+    }
 }
