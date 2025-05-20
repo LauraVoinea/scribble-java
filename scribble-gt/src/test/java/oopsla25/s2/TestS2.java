914294c87d6a6c4cc0c5fc70cc39174befe7aec3
rhu1 <r.hu@qmul.ac.uk>
2025-05-20 18:59:48 +0100

    Adding tests

---
+++ scribble-gt/src/test/java/oopsla25/s2/TestS2.java
@@ -0,0 +1,33 @@
+package oopsla25.s2;
+
+import oopsla25.TestUtil;
+import org.junit.Test;
+import org.scribble.ext.gt.cli.GTCommandLine2;
+
+public class TestS2 {
+
+    @Test
+    public void testTwoPartyChoice() {
+        TestUtil.testGood(TestUtil.BASE_PATH + "\\s2\\good\\TwoPartyChoice.scr");
+    }
+
+    @Test
+    public void testTwoPartyChoiceNonDirected() {
+        TestUtil.testBad(TestUtil.BASE_PATH + "\\s2\\bad\\TwoPartyChoiceNonDirected.scr");
+    }
+
+    @Test
+    public void testTwoPartyMC() {
+        TestUtil.testGood(TestUtil.BASE_PATH + "\\s2\\good\\TwoPartyMC.scr");
+    }
+
+    @Test
+    public void testTimeout() {
+        TestUtil.testGood(TestUtil.BASE_PATH + "\\s2\\good\\Timeout.scr");
+    }
+
+    @Test
+    public void testWFAmbiguous() {
+        TestUtil.testBad(TestUtil.BASE_PATH + "\\s2\\bad\\WFAmbiguous.scr");
+    }
+}
