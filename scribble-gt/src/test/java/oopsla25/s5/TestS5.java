6d8ea388e1e5983ad45cb3dd08691f60ea268853
rhu1 <r.hu@qmul.ac.uk>
2025-05-21 12:15:49 +0100

    Continue tests

--- scribble-gt/src/test/java/oopsla25/s5/TestS5.java
+++ scribble-gt/src/test/java/oopsla25/s5/TestS5.java
@@ -24,4 +24,47 @@ public class TestS5 {
     public void testAMQP_long() {
         TestUtil.testGood(TestUtil.BASE_PATH + "\\s5\\good\\AMQP_long.scr");
     }
+
+
+    /* Table 1 */
+
+    @Test
+    public void testCalculator() {
+        TestUtil.testGood(TestUtil.BASE_PATH + "\\s5\\good\\table1\\Calculator.scr");
+    }
+
+    @Test
+    public void testCircuitBreaker() {
+        TestUtil.testGood(TestUtil.BASE_PATH + "\\s5\\good\\table1\\CircuitBreaker.scr");
+    }
+
+    @Test
+    public void testDistributedLogging() {
+        TestUtil.testGood(TestUtil.BASE_PATH + "\\s5\\good\\table1\\DistributedLogging.scr");
+    }
+
+    @Test
+    public void testFibonacci() {
+        TestUtil.testGood(TestUtil.BASE_PATH + "\\s5\\good\\table1\\Fibonacci.scr");
+    }
+
+    @Test
+    public void testSMTP() {
+        TestUtil.testGood(TestUtil.BASE_PATH + "\\s5\\good\\table1\\SMTP.scr");
+    }
+
+    @Test
+    public void testTwoBuyer() {
+        TestUtil.testGood(TestUtil.BASE_PATH + "\\s5\\good\\table1\\TwoBuyer.scr");
+    }
+
+    @Test
+    public void testTravelAgency() {
+        TestUtil.testGood(TestUtil.BASE_PATH + "\\s5\\good\\table1\\TravelAgency.scr");
+    }
+
+    @Test
+    public void testOnlineWallet() {
+        TestUtil.testGood(TestUtil.BASE_PATH + "\\s5\\good\\table1\\OnlineWallet.scr");
+    }
 }
